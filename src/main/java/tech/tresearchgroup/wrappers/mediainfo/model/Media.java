b5fc3bac4435735b5b9646461eef0ffee8c09113
Jared Thorne <jthorne@tresearchgroup.ca>
2022-05-16 14:56:00 -0400

    Changed version number and added lombok to model

--- src/main/java/tech/tresearchgroup/wrappers/mediainfo/model/Media.java
+++ src/main/java/tech/tresearchgroup/wrappers/mediainfo/model/Media.java
@@ -2,7 +2,13 @@ package tech.tresearchgroup.wrappers.mediainfo.model;
 
 import java.util.List;
 import com.google.gson.annotations.SerializedName;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Media{
 
 	@SerializedName("track")
--- src/main/java/tech/tresearchgroup/wrappers/mediainfo/model/MediaInfoOutput.java
+++ src/main/java/tech/tresearchgroup/wrappers/mediainfo/model/MediaInfoOutput.java
@@ -1,7 +1,13 @@
 package tech.tresearchgroup.wrappers.mediainfo.model;
 
 import com.google.gson.annotations.SerializedName;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class MediaInfoOutput{
 
 	@SerializedName("creatingLibrary")
